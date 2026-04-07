3dd53e695b6998c402053268d219d50b88f3b14a
LostFlashlight <miner2000@outlook.de>
2026-04-07 16:33:19 +0200

    really fix rclone install script

--- files/scripts/get_rclone-manager.sh
+++ files/scripts/get_rclone-manager.sh
@@ -3,4 +3,4 @@
 
 URL="$(curl -sL https://github.com/Zarestia-Dev/rclone-manager/releases/latest/download/latest.json | jq '.platforms."linux-x86_64-rpm".url' -r)"
 curl $URL -Lo rcm.rpm
-dnf install -y /tmp/rcm.rpm
+dnf install -y ./rcm.rpm
