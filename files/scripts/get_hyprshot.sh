b1cb3a7515980869fcb212377658c8c9ebf6929b
LostFlashlight <miner2000@outlook.de>
2026-07-12 19:28:20 +0200

    adapt to the wayblue copr

--- files/scripts/get_hyprshot.sh
+++ files/scripts/get_hyprshot.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # script for installing hyprshot (screenshot tool (cli only))
-cd /usr/share
 git clone https://github.com/Gustash/hyprshot.git Hyprshot
-ln -s /usr/share/Hyprshot/hyprshot /usr/bin
-chmod +x Hyprshot/hyprshot
+cp Hyprshot/hyprshot /usr/bin
+chmod +x /usr/bin/hyprshot
